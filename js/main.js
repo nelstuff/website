75ae9af250c8adfcb2eb99463f5018f73dd0111b
nelstuff <43881917+nelstuff@users.noreply.github.com>
2020-05-09 11:12:41 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var numImages = 6;
-    int index = Math.floor(Math.random() * numImages);
+    var index = Math.floor(Math.random() * numImages);
     var changeEvery = 7000;
     
     function displayNextImage() {
